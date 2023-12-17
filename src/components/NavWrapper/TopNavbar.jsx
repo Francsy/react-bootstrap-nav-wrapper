55c187d9b27e71773da3db270d30ca0d2163e88d
Francsy <francsyludens@gmail.com>
2023-12-17 01:06:21 +0100

    fix/toggle button

--- src/components/NavWrapper/TopNavbar.jsx
+++ src/components/NavWrapper/TopNavbar.jsx
@@ -7,13 +7,13 @@ const TopNavbar = React.forwardRef(({ isMobile, toggleSidebar }, ref) => {
     <header className="fixed-top">
       <Navbar bg="primary" expand="lg" className="bg-body-tertiary top-menu" ref={ref}>
         <Container fluid>
-          {isMobile && <Navbar.Toggle onClick={toggleSidebar} />}
+          {isMobile && <Navbar.Toggle onClick={toggleSidebar} className="toggle-button" />}
           <Navbar.Brand href="#home">
             <img
               src="/logo.svg"
               width="60"
               height="60"
-              className="d-inline-block align-top"
+              className="d-inline-block align-top top-logo"
               alt="Logo Francsy"
             />
           </Navbar.Brand>
